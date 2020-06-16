19c5239a3c73885777be21616ded07c9cd94988c
shirzadbasira1@gmail.com <basirashirzad@DESKTOP-OI54QJJ.localdomain>
2020-06-16 13:37:41 -0400

    Filter completed

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -2,5 +2,8 @@
 
 # filter original 1gram file googlebooks-eng-all-1gram-20120701-1.gz to only lines where the ngram exactly matches a year (18xx, 19xx, or 20xx, where x is a digit)
 #   decompress the first using gunzip, zless, zcat or similar
+    gunzip googlebooks-eng-all-1gram-20120701-1.gz
+    
 #   then filter out rows that match using grep -E, egrep, awk, or similar
 #   write results to year_counts.tsv
+    grep -E "18|19|20" googlebooks-eng-all-1gram-20120701-1 > year_counts.tsv
